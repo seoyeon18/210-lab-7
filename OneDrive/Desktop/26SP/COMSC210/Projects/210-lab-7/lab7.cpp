d65d2ecf733627f16a49d4e8cdeced01abd6cb56
Bright <brightsy04@gmail.com>
2026-02-15 20:40:30 -0800

    lab-7-2

--- OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-7/lab7.cpp
+++ OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-7/lab7.cpp
@@ -11,6 +11,24 @@ int main()
 
     string* names = new string[SIZE];
 
+    *(names + 0) = "Janet";
+    *(names + 1) = "Jeffe";
+    *(names + 2) = "Jin";
+    *(names + 3) = "Joe";
+    *(names + 4) = "Junio";
+
+    cout << "Original array: ";
+    displayArray(names, SIZE);
+
     delete[] names;
     return 0;
+}
+
+void displayArray(string* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(arr + i);
+    }
+    cout << endl;
 }
