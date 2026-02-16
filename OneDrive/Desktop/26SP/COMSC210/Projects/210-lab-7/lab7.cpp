40ba707e0774bd09939f555597b35cf4f6680e83
Bright <brightsy04@gmail.com>
2026-02-15 20:52:25 -0800

    lab-7-3

--- OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-7/lab7.cpp
+++ OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-7/lab7.cpp
@@ -20,8 +20,24 @@ int main()
     cout << "Original array: ";
     displayArray(names, SIZE);
 
+
+    reverseArray(names, SIZE);
+
+     cout << "Reversed array: ";
+    displayArray(names, SIZE);
+
     delete[] names;
     return 0;
+}
+string* reverseArray(string* arr, int size)
+{
+    for (int i = 0; i < size / 2; i++)
+    {
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + (size - 1 - i));
+        *(arr + (size - 1 - i)) = temp;
+    }
+    return arr;
 }
 
 void displayArray(string* arr, int size)
